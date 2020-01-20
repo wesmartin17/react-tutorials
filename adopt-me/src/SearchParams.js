5d7ec6bc6cc76c9cd13e9d448213baada887e344
Wes <wes.martin17@gmail.com>
2020-01-20 11:23:41 -0500

    search params

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breeds, setBreeds] = useState([]);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  //Runs after SearchParams actually renders
+  //Make sure you decalre all dependencies of useEffect!
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      //Take just the name from the breed object returned by the api
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+    //useeffect is only called when one of these is changed
+  }, [animal, setBreed, setBreeds]);
+
   return (
     <div className="search-params">
       <form>
